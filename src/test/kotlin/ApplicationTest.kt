c17f27c149d91d5cf091a5eebb74d378e3769cb3
Joakim Taule Kartveit <joakimkartveit@gmail.com>
2025-05-23 20:14:14 +0200

    chore: fix tests

--- src/test/kotlin/ApplicationTest.kt
+++ src/test/kotlin/ApplicationTest.kt
@@ -1,21 +1,37 @@
 package mikaojk.github.io
 
+import com.fasterxml.jackson.databind.ObjectMapper
+import com.fasterxml.jackson.databind.SerializationFeature
+import com.fasterxml.jackson.module.kotlin.registerKotlinModule
 import io.ktor.client.request.*
 import io.ktor.http.*
+import io.ktor.http.HttpHeaders.Accept as AcceptHeader
+import io.ktor.http.HttpHeaders.ContentType as ContentTypeHeader
 import io.ktor.server.testing.*
 import kotlin.test.Test
 import kotlin.test.assertEquals
 
 class ApplicationTest {
 
+    private val objectMapper: ObjectMapper =
+        ObjectMapper()
+            .registerKotlinModule()
+            .configure(SerializationFeature.WRITE_DATES_AS_TIMESTAMPS, false)
+
+
     @Test
-    fun testRoot() = testApplication {
+    fun testGenerateQr() = testApplication {
         application {
             module()
         }
-        client.post("/").apply {
-            assertEquals(HttpStatusCode.OK, status)
-        }
+
+        val response =
+            client.post("/generate-qr") {
+                setBody(objectMapper.writeValueAsString(QRCodeRequest("www.google.com")))
+            }
+
+        assertEquals(HttpStatusCode.OK, response.status)
+
     }
 
 }
