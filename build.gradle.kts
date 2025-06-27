dfe6099e7b2fbee45964c5888a3a535d45b0442a
Joakim Taule Kartveit <joakimkartveit@gmail.com>
2025-06-27 20:01:40 +0200

    Update build.gradle.kts

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,11 +1,11 @@
 val javaVersion = 21
 
-val kotlinVersion = "2.1.21"
+val kotlinVersion = "2.2.0"
 val logbackVersion = "1.5.18"
 val zingVersion = "3.5.3"
 
 plugins {
-    kotlin("jvm") version "2.1.21"
+    kotlin("jvm") version "2.2.0"
     id("io.ktor.plugin") version "3.2.0"
 }
 
@@ -27,19 +27,19 @@ repositories {
 }
 
 dependencies {
-    // KtorSever dependencies
+    // Ktor server
     implementation("io.ktor:ktor-server-core")
     implementation("io.ktor:ktor-server-cio")
     implementation("io.ktor:ktor-serialization-jackson")
     implementation("io.ktor:ktor-server-content-negotiation")
 
-    // Logging dependencies
+    // Logging
     implementation("ch.qos.logback:logback-classic:$logbackVersion")
 
-    // QR code dependencies
+    // QR code
     implementation("com.google.zxing:javase:$zingVersion")
 
-    // Test dependencies
+    // Test
     testImplementation("io.ktor:ktor-server-test-host")
     testImplementation("org.jetbrains.kotlin:kotlin-test-junit:$kotlinVersion")
 }
